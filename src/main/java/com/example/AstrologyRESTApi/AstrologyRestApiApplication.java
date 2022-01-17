55462bfb1f3d121efd859fa5270a2a51fc246df0
BrookeBryski <brookeabryski@gmail.com>
2022-01-17 11:54:33 -0600

    initial commit

---
+++ src/main/java/com/example/AstrologyRESTApi/AstrologyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AstrologyRESTApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AstrologyRESTApi/AstrologyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AstrologyRESTApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrologyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
